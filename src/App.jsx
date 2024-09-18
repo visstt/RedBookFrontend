473ff1c671dfede21e83bb29873a269e2cb1025a
Admin <@schckuratova.mar@yandex.ru>
2024-09-18 16:56:35 +0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Info from './components/pages/Info/Info'
+
+function App() {
+
+  return (
+    <>
+      <Info/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/pages/Info/Info.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./Info.module.css";
+
+export default function Info() {
+  return (
+    <div className={styles.InfoPage}>
+      <div className={styles.Text}>
+        <h1 className={styles.h1}>О НАС</h1>
+      </div>
+      <hr className={styles.line} />
+
+      <div className={styles.InfSection}>
+        <img src="public/img/img.png" alt="123" className={styles.Photo} />
+        <h2 className={styles.TextInfo}>
+          Расположен на северо-западе и западе столицы и находится в основном на
+          незастроенной территории, протянувшейся вдоль берегов Москвы-реки от
+          Строгина до Филёвской поймы. Площадь парка — 3660 гектаров, что
+          составляет 3,5 % от общей площади города и 23 % от всех московских
+          ООПТ. Парк включает в себя 22 экологические зоны, отделенные друг от
+          друга реками, заливами, каналами и улицами. 
+        </h2>
+      </div>
+    </div>
+  );
+}
